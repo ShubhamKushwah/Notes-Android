0bee98cce143c7a7457fdd1a386ba029ed957716
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-08-10 21:42:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/project/notes/FingerPrintHandler.java
@@ -0,0 +1,39 @@
+package com.project.notes;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+class FingerPrintHandler extends FingerprintManager.AuthenticationCallback {
+
+    private Context context;
+
+    public FingerPrintHandler(Context context) {
+        this.context = context;
+    }
+
+    public void startAuthentication(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject) {
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED)
+            return;
+
+        fingerprintManager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
+        context.startActivity(new Intent(context, HomeActivity.class));
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        super.onAuthenticationFailed();
+        Toast.makeText(context, "Fingerprint authentication failed!", Toast.LENGTH_SHORT).show();
+    }
+}
